66acb261e0f72062025ffc084164e2938f593036
Marcelo rocha <marcelo.logus3414@gmail.com>
2022-10-28 16:16:38 -0300

    Atualização

---
+++ Eletroeletronica/Eletroeletronica/Estoque.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eletroeletronica
+{
+    internal class Estoque
+    {
+        private int quantidade;
+        private string nome;
+        private string descricao;
+
+        public int Quantidade
+        {
+            get { return Quantidade; }
+            set { Quantidade = value; }
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Descricao
+        {
+            get { return Descricao; }
+            set { Descricao = value; }
+        }
+
+    }
+}
